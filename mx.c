558348b0443ef3c618760aee8c27388ec21c7820
Richard Russon <rich@flatcap.org>
2018-05-07 15:33:51 +0100

    empty help mailbox

--- mx.c
+++ mx.c
@@ -45,6 +45,7 @@
 #include "context.h"
 #include "copy.h"
 #include "globals.h"
+#include "help/help.h"
 #include "hook.h"
 #include "keymap.h"
 #include "mailbox.h"
@@ -127,6 +128,8 @@ struct MxOps *mx_get_ops(int magic)
     case MUTT_NOTMUCH:
       return &mx_notmuch_ops;
 #endif
+    case MUTT_HELP:
+      return &mx_help_ops;
     default:
       return NULL;
   }
@@ -210,6 +213,16 @@ bool mx_is_nntp(const char *p)
 }
 #endif
 
+/**
+ * mx_is_help - Is this a Help mailbox
+ * @param p Mailbox string to test
+ * @retval true It is a Help mailbox
+ */
+bool mx_is_help(const char *p)
+{
+  return (url_check_scheme(p) == U_HELP);
+}
+
 #ifdef USE_NOTMUCH
 /**
  * mx_is_notmuch - Is this a Notmuch mailbox
@@ -247,6 +260,9 @@ int mx_get_magic(const char *path)
   if (mx_is_imap(path))
     return MUTT_IMAP;
 #endif /* USE_IMAP */
+
+  if (mx_is_help(path))
+    return MUTT_HELP;
 
 #ifdef USE_POP
   if (mx_is_pop(path))
